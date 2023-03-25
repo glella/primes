94c3a7eb4ae7f51f9c9726b0626897bbe648c16c
Guillermo Lella <arkorott@gmail.com>
2023-03-25 12:40:44 -0400

    updated Rust channels new version
    Added better error handling and generics plus an improved trial division algorithm that is much faster and thus not comparable in this 2nd version to the other languages

---
+++ Rust - Channels/primes2/target/debug/deps/primes2-3131003d098c7c78.d
@@ -0,0 +1,5 @@
+/Users/guillermolella/OneDrive/Programming/Git/primes/Rust - Channels/primes2/target/debug/deps/primes2-3131003d098c7c78: src/main.rs
+
+/Users/guillermolella/OneDrive/Programming/Git/primes/Rust - Channels/primes2/target/debug/deps/primes2-3131003d098c7c78.d: src/main.rs
+
+src/main.rs:
