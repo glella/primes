c877c5dbc4f40d49dcdc59140f10ac9919b02768
Guillermo Lella <arkorott@gmail.com>
2023-03-28 20:49:54 -0400

    update rust

---
+++ Rust - Channels/primes2/target/x86_64-pc-windows-gnu/release/deps/primes2-29e4db95356d00d9.d
@@ -0,0 +1,6 @@
+/Users/guillermolella/OneDrive/Programming/Git/primes/Rust - Channels/primes2/target/x86_64-pc-windows-gnu/release/deps/primes2-29e4db95356d00d9.exe: src/main.rs src/common.rs
+
+/Users/guillermolella/OneDrive/Programming/Git/primes/Rust - Channels/primes2/target/x86_64-pc-windows-gnu/release/deps/primes2-29e4db95356d00d9.d: src/main.rs src/common.rs
+
+src/main.rs:
+src/common.rs:
